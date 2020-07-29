67e79997bd8e21323986fc0fe4c87cf1cdad5356
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-07-29 13:44:56 +0530

    Updates

---
+++ precache-manifest.0f729c18bf8e87282f7c095c4af7edfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be6b509bb95d94fd71b9379a16436d4",
+    "url": "/hooksBookList/index.html"
+  },
+  {
+    "revision": "005d8615d70c27c394ad",
+    "url": "/hooksBookList/static/css/main.89429ac5.chunk.css"
+  },
+  {
+    "revision": "fd903da92d40a72c1b7f",
+    "url": "/hooksBookList/static/js/2.45be473a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooksBookList/static/js/2.45be473a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005d8615d70c27c394ad",
+    "url": "/hooksBookList/static/js/main.5f6b5633.chunk.js"
+  },
+  {
+    "revision": "675d3a60e2470600165d",
+    "url": "/hooksBookList/static/js/runtime-main.c382c0a1.js"
+  }
+]);
